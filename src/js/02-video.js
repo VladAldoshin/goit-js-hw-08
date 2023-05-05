fda9d1c64e74f8ede1938b428662d75db3cfe7b0
VladAldoshin <aldoshinvladislav87@gmail.com>
2023-05-05 18:50:12 +0300

    update video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@ import Player from '@vimeo/player';
 import  throttle  from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player('iframe');
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
@@ -10,4 +10,4 @@ function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')));
